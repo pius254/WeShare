94dc3996d26fdc5a8642a06d980d585cbbafdb45
Pius Wanjiru <piusmuriithi1@gmail.com>
2017-06-27 12:52:28 +0300

    Add a notification partial to display any success or error messages

---
+++ resources/views/partials/notification.blade.php
@@ -0,0 +1,14 @@
+@if(Session::has('success'))
+<div class="alert alert-success">
+<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+    <p class="alert">{{ Session::get('success') }}</p>
+</div>
+@endif
+
+
+@if(Session::has('error'))
+    <div class="alert alert-danger">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <p class="alert">{{ Session::get('error') }}</p>
+    </div>
+@endif
